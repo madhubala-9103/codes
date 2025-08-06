4abf88a7881a41e2d9346836dc2cc5344d4660aa
madhubala-9103 <madhubala9103@mountzion.ac.in>
2025-08-06 16:10:39 +0530

    Create madhu.r

---
+++ madhu.r
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class sample{
+    public static void main(String[]args){
+        Scanner scan=new Scanner(System.in);
+        int a = scan.nextInt();
+        int sum=0;
+        int i=0;
+        while(i<=a){
+            if(i%2==0){
+            sum=sum+i;}
+            i++;
+        }
+        
+        
+        if(sum%2==0){
+        System.out.println("is even");
+        
+    }
+    else{
+        System.out.println("is not odd");
+}
+}
